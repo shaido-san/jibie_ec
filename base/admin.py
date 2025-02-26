5b2b82b1f9cad8387849e0117c3b2970d507768a
saito4 <hiroto.saitou1201@gmail.com>
2025-02-26 21:45:47 +0900

    注文履歴確認追加

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,17 @@
 from django.contrib import admin
-from .models import Item, Stock
+from .models import Item, Stock, Order, OrderItem
 
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "address", "total_price", "created_at")  # 一覧表示
+    list_filter = ("created_at",)  # 作成日でフィルタリング
+    search_fields = ("user__username", "address__name")  # 検索フィールド
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("order", "item", "quantity", "subtotal_price")  # 一覧表示
+    list_filter = ("order",)  # 注文ごとにフィルタリング
+    search_fields = ("item__name",)  # 商品名で検索
 class StockInLine(admin.StackedInline):
     model = Stock
     extra = 1
